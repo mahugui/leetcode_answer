896ebe57d19f341e96c4618e2cb79ef4926f4648
zhangyeshuo <zhangyeshuo@xuetangx.com>
2018-03-09 16:27:35 +0800

    twosum add another func

--- twosum.py
+++ twosum.py
@@ -15,6 +15,7 @@ return [0, 1].
 
 def two_sum(nums, target):
     """
+    这种方法多次遍历了nums,时间复杂度是高
     :type nums: List[int]
     :type target: int
     :rtype: List[int]
@@ -29,8 +30,29 @@ def two_sum(nums, target):
     return []
 
 
+def two_sum_use_dict(nums, target):
+    """
+    :type nums: List[int]
+    :type target: int
+    :rtype: List[int]
+    """
+
+    if len(nums) <= 1:
+        return []
+
+    buff_dict = {}
+    for index, value in enumerate(nums):
+        difference = target - value
+        if difference in buff_dict:
+            return [buff_dict[difference], index]
+        else:
+            buff_dict[value] = index
+    return buff_dict.values()
+
+
 if __name__ == "__main__":
     nums = [11, 7, 8, 1, 2]
     target = 9
     result = two_sum(nums, target)
     print(result)
+    print(two_sum_use_dict(nums, target))
